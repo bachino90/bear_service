d10240d734210902956eef6597cee4e052fcce14
bachino90 <emilianobivachi@gmail.com>
2014-09-19 17:57:00 -0300

    building client controller

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon         = require('serve-favicon');
 var logger          = require('morgan');
 var cookieParser    = require('cookie-parser');
 var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var mongoose        = require('mongoose');
+var helpers         = require('express-helpers');
 
-var routes            = require('./routes/index');
-var users             = require('./routes/users');
 var beaconController  = require('./controllers/beacon_controller');
 var clientController  = require('./controllers/client_controller');
 var storeController   = require('./controllers/store_controller');
@@ -14,34 +15,45 @@ var areaController    = require('./controllers/area_controller');
 var loginController   = require('./controllers/login_controller');
 var app               = express();
 
-//===========================================================================================//
-//====== App CONFIGURATION ==================================================================//
-//===========================================================================================//
+//=============================================================================================================//
+//====== App CONFIGURATION ====================================================================================//
+//=============================================================================================================//
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+helpers(app);// set view helpers for EJS
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride()); 				// simulate DELETE and PUT
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//===========================================================================================//
-//====== Controllers CONFIGURATION ==========================================================//
-//===========================================================================================//
+//===============================//
+//====== DB CONFIGURATION =======//
+//===============================//
+
+console.log(process.env.MONGOHQ_URL);
+//'mongodb://heroku:-IY7qZns1g-mcGfYpV29JVjwjMmmYu_v5ITVPCW1gPQjCYakIOrrmq7z0lE3PelCJJj3GgCLkohxAB28LzWF5Q@kahana.mongohq.com:10092/app26226186';
+var mongoUri = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI ||'mongodb://localhost/beardb';
+mongoose.connect(mongoUri); // connect to our database
+
+//=============================================================================================================//
+//====== Controllers CONFIGURATION ============================================================================//
+//=============================================================================================================//
 
 app.use('/', loginController);
 app.use('/clients', clientController);
 app.use('/clients/:clientId/stores', storeController);
 app.use('/clients/:clientId/stores/:storeId/areas', areaController);
 
-//===========================================================================================//
-//====== Error Handlers CONFIGURATION =======================================================//
-//===========================================================================================//
+//=============================================================================================================//
+//====== Error Handlers CONFIGURATION =========================================================================//
+//=============================================================================================================//
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/client_controller.js
+++ controllers/client_controller.js
@@ -24,19 +24,45 @@ function isLoggedIn(req, res, next) {
   return next();
 }
 
+function insideRedirectToHome(req,res,rN,m,vE,newB,showB,b,c) {
+  if (vE['code'] == 11000) {
+    m = "Already exist";
+  }
+  var title = "Beacons";
+  var client_id = "";
+  if (req.params.client_id) {
+    title = b[0].name;
+    client_id = req.params.client_id;
+  }
+  res.render('beacons/index',{title: title,
+                       clients_side: c,
+                       clients_main: b,
+                          client_id: client_id,
+                           routeNew: rN,
+                            message: m,
+                             valErr: vE,
+                          newBeacon: newB,
+                         showBeacon: showB});
+}
+
 // GET /clients
 router.get('/', isLoggedIn, function(req, res) {
-  redirectToHomeWithErrors(req,res,false,'',false);
+  BeaconClient.find(function(err, all_clients){
+    if (err) {
+      res.render(err);
+    }
+    res.render('show_clients',{ clients: all_clients });
+  });
 });
 
 // GET /clients/:client_id
 router.get('/:client_id',isLoggedIn, function(req, res){
-  redirectToHomeWithErrors(req,res,false,'',false);
-});
-
-// GET /clients/new
-router.get('/new', isLoggedIn, function(req, res) {
-  redirectToHomeWithErrors(req,res,true,'',false);
+  BeaconClient.findById(req.params.client_id, function(err, client){
+    if (err) {
+      res.render(err);
+    }
+    res.render('client',{ client: client });
+  });
 });
 
 // POST /clients
@@ -50,5 +76,25 @@ router.post('/', isLoggedIn, function(req,res) {
       res.render(err);
     }
     res.redirect('/clients');
+  });
+});
+
+// PUT /clients
+router.put('/:client_id', isLoggedIn, function(req,res) {
+  // use our bear model to find the bear we want
+  BeaconClient.findById(req.params.client_id, function(err, client) {
+    if (err) {
+      res.render(err);
+    }
+    client.name = req.body.name;
+    client.mayor_id = req.body.primary_uuid;
+    client.minor_id = req.body.secondary_uuid;
+    // save the client update
+    client.save(function(err) {
+      if (err) {
+        res.render(err);
+      }
+      res.redirect('/client');
+    });
   });
 });
