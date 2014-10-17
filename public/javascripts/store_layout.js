4d8ddbe875b74431c53406e2ff325c5f44ea8d68
bachino90 <emilianobivachi@gmail.com>
2014-10-17 01:12:10 -0300

    building layout

--- public/javascripts/store_layout.js
+++ public/javascripts/store_layout.js
@@ -1,40 +1,87 @@
+Array.prototype.max = function() {
+  return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function() {
+  return Math.min.apply(null, this);
+};
 
 function drawGrid() {
+  var c = $('#canvas_layout');
+  var ct = c.get(0).getContext('2d');
+  var container = $(c).parent();
+  c.attr('width', $(container).width()); //max width
+  c.attr('height', $(container).height()); //max height
+
   var canvas = document.getElementById("canvas_layout");
   var context = canvas.getContext("2d");
   var origin_x = 0.0;
   var origin_y = 30.0;
-  var width = 50.0;
+  var scale = 50.0;
+  var delta = 50.5;
+  var canvasWidth = canvas.width;
+  var canvasHeight = canvas.height;
 
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  var max = Math.max(canvasWidth, canvasHeight);
+  var numberOfRules = parseInt(max/scale) + 1;
+
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
   context.beginPath();
-  context.lineWidth=2;
+  context.lineWidth=0.2;
 
-  for (var i=1;i<corners_x.length;i++) {
-    var x = x + width;
-    var y = origin_y + parseInt(corners_y[i]);
-    context.lineTo(x, y);
+  for (var i=1;i<=numberOfRules;i++) {
+    context.moveTo(delta,0);
+    context.lineTo(delta, canvasHeight);
+
+    context.moveTo(0,delta);
+    context.lineTo(canvasWidth, delta);
+
+    delta += scale;
   }
+  context.stroke();
 }
 
 
 $(document).ready(function() {
+  /*
+  //Get the canvas &
+  var c = $('#canvas_layout');
+  var ct = c.get(0).getContext('2d');
+  var container = $(c).parent();
+  //Run function when browser resizes
+  $($('#canvas_layout').parent()).resize(respondCanvas);
+
+  function respondCanvas(){
+    var w = $($('#canvas_layout').parent()).width();
+    var h = $($('#canvas_layout').parent()).height();
+    c.attr('width', $(container).width()); //max width
+    c.attr('height', $(container).height()); //max height
+    //Call a function to redraw other content (texts, images etc)
+    drawGrid();
+  }
+  //Initial call
+  respondCanvas();
+
   $('input#deleteCornerButton').click(function (){
     $(this).parent().parent().remove();
   });
 
+  $('button#editLayoutButton').click(function(){
+    respondCanvas();
+  });
+  */
   $('#addCornerButton').click(function (){
     $('#corners').append("<div class='col-lg-12'>"+
       "<div class='col-sm-5' style='margin-bottom:15px;'>"+
         "<div class='input-group'>"+
           "<div class='input-group-addon'>X</div>"+
-          "<input type='text' class='form-control' value='' placeholder='1' name='store_name' required>"+
+          "<input type='text' class='form-control' id='x' value='' placeholder='1' name='store_name' required>"+
         "</div>"+
       "</div>"+
       "<div class='col-sm-5' style='margin-bottom:15px;''>"+
         "<div class='input-group'>"+
           "<div class='input-group-addon'>Y</div>"+
-          "<input type='text' class='form-control' value='' placeholder='1' name='store_name' required>"+
+          "<input type='text' class='form-control' id='y' value='' placeholder='1' name='store_name' required>"+
         "</div>"+
       "</div>"+
       "<div class='col-sm-2' style='margin-bottom:15px;'>"+
@@ -76,27 +123,31 @@ $(document).ready(function() {
   $('#generateView').click(function(){
     var canvas = document.getElementById("canvas_layout");
     var context = canvas.getContext("2d");
-    var origin_x = 30.0;
-    var origin_y = 30.0;
+    var origin_x = 30.5;
+    var origin_y = 30.5;
     var corners_x = $('input#x').map(function (){
-      return $( this ).val();
+      return parseInt($( this ).val());
     }).get();
     var corners_y = $('input#y').map(function (){
-      return $( this ).val();
+      return parseInt($( this ).val());
     }).get();
 
     context.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
     context.beginPath();
     context.lineWidth=2;
-    context.moveTo(origin_x + parseInt(corners_x[0]), origin_y + parseInt(corners_y[0]));
+    context.moveTo(origin_x + corners_x[0], origin_y + corners_y[0]);
+
+    var maxX = corners_x.max();
+    var maxY = corners_y.max();
+
+    var scale = canvas.height/(2*origin_x + maxX);//1;
 
     for (var i=1;i<corners_x.length;i++) {
-      var x = origin_x + parseInt(corners_x[i]);
-      var y = origin_y + parseInt(corners_y[i]);
+      var x = origin_x + corners_x[i] * scale;
+      var y = origin_y + corners_y[i] * scale;
       context.lineTo(x, y);
     }
-
-
     context.stroke();
 
   });
